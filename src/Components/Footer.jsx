558b326604b460ab200668f25bea0dabf3003dea
Paulo de Tarso <paulo.oinab@gmail.com>
2022-07-15 14:55:30 -0300

    Criando menu inferior

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import drinkIcon from '../images/drinkIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
+
+const Footer = () => (
+  <footer data-testid="footer">
+    <Link to="/foods">
+      <img data-testid="food-bottom-btn" src={ mealIcon } alt="Meals" />
+    </Link>
+    <Link to="/drinks">
+      <img data-testid="drinks-bottom-btn" src={ drinkIcon } alt="Drinks" />
+    </Link>
+  </footer>
+);
+
+export default Footer;
---
+++ src/Pages/Drinks.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Footer from '../Components/Footer';
+
+const Drinks = () => (
+  <>
+    <h1>Drinks</h1>
+    <Footer />
+  </>
+);
+
+export default Drinks;
